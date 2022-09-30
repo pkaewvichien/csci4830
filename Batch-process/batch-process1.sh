9ae5c9b3dbdbd7080285d971fc24409fb3cb1356
pkaewvichien <114364924+pkaewvichien@users.noreply.github.com>
2022-09-30 16:45:23 -0500

    Add files via upload

---
+++ Batch-process/batch-process1.sh
@@ -0,0 +1,10 @@
+#! /bin/sh 
+for f in "$1"/*; do
+  if [ -f "$f" ]; then
+    SIZE="$(du -sh "${f}" | cut -f1)"
+    echo "Processing $f file..."
+    echo "$SIZE"
+    echo "$WORDCOUNT"
+  fi
+done
+
