ce9348b0f314124582066c1739eb29bcecaaed76
pkaewvichien <114364924+pkaewvichien@users.noreply.github.com>
2022-09-30 12:27:44 -0500

    Add files via upload

---
+++ workspaceCSCI4830-ex-0919-Kaewvichien/webprojectCSCI4830-ex-0919_Kaewvichien/src/util/Info.java
@@ -0,0 +1,7 @@
+package util;
+
+public interface Info {
+   public String projectName = "webprojectCSCI4830-ex-0919_Kaewvichien";
+   public String searchWebName = "simpleSearchHB.html";
+   public String insertWebName = "simpleInsertHB.html";
+}
